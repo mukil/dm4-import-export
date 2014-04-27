e68bcba1902a62ffd54a5fbac4e70956471c1229
carolina <carolina@sindominio.net>
2014-04-27 15:16:34 +0200

    Add separator and new import item to topicmap menu

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,14 +1,13 @@
 dm4c.add_plugin("net.abriraqui.import-export", function(){
-
-    var topicmap_menu =dm4c.get_plugin("de.deepamehta.topicmaps").get_topicmap_menu()
-    topicmap_menu.add_item({
-	label: "Import Topicmap",
-	handler: import_topicmap
+    dm4c.add_listener("post_refresh_topicmap_menu", function(topicmap_menu) {
+	topicmap_menu.add_separator()
+	topicmap_menu.add_item({
+	    label: "Import Topicmap",
+	    handler: import_topicmap
+	})
+	
+	function import_topicmap(){
+	    alert("Import topicmap XX")
+	}
     })
-
-    function import_topicmap(){
-	alert("Import topicmap XX")
-    }
-
-
 })
