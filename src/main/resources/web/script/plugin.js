a39dedd7426b927f615b1a37d69763543a29997f
carolina <carolina@sindominio.net>
2014-04-28 01:27:40 +0200

    Add topicmap id to import alert box on topicmap menu

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -1,4 +1,5 @@
 dm4c.add_plugin("net.abriraqui.import-export", function(){
+
     dm4c.add_listener("post_refresh_topicmap_menu", function(topicmap_menu) {
 	topicmap_menu.add_separator()
 	topicmap_menu.add_item({
@@ -7,7 +8,8 @@ dm4c.add_plugin("net.abriraqui.import-export", function(){
 	})
 	
 	function import_topicmap(){
-	    alert("Import topicmap XX")
+	    var topicmap = dm4c.get_plugin("de.deepamehta.topicmaps").get_topicmap()
+	    alert("Import topicmap" + topicmap.get_id())
 	}
     })
 })
