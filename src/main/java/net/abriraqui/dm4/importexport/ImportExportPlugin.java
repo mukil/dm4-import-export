c7233ab343315d2e59de294569889786f38ffe03
carolina <carolina@sindominio.net>
2014-05-03 13:58:40 +0200

    Add simple export topicmap method

---
+++ src/main/java/net/abriraqui/dm4/importexport/ImportExportPlugin.java
@@ -0,0 +1,61 @@
+package net.abriraqui.dm4.importexport;
+
+import de.deepamehta.core.osgi.PluginActivator;
+import de.deepamehta.core.service.ClientState;
+import de.deepamehta.core.service.PluginService;
+import de.deepamehta.core.service.annotation.ConsumesService;
+
+import de.deepamehta.plugins.topicmaps.service.TopicmapsService;
+import de.deepamehta.plugins.topicmaps.model.TopicmapViewmodel;
+
+import java.io.Writer;
+import java.io.FileWriter;
+import java.util.logging.Logger;
+
+import org.codehaus.jettison.json.JSONObject;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.CookieParam;
+
+@Path("/import-export")
+public class ImportExportPlugin extends PluginActivator {
+    
+
+    private TopicmapsService topicmapsService;
+
+    private Logger log = Logger.getLogger(getClass().getName());
+
+    // Service implementation //
+
+    @POST
+    @Path("/export")
+    public void export(@CookieParam("dm4_topicmap_id") long topicmapId) {
+	try {
+	    log.info("Exporting topicmap #########" + topicmapId);
+	    TopicmapViewmodel topicmap = topicmapsService.getTopicmap(topicmapId);
+	    Writer writer = new FileWriter("topicmap-" + topicmapId + ".json");
+	    JSONObject json = topicmap.toJSON();
+	    json.write(writer);
+	    writer.close();
+	} catch (Exception e) {
+	    throw new RuntimeException("Export failed", e );
+	}
+    }
+
+    // Hook implementation //
+    
+    @Override
+    @ConsumesService("de.deepamehta.plugins.topicmaps.service.TopicmapsService")
+    public void serviceArrived(PluginService service) {
+	topicmapsService = (TopicmapsService) service;
+    }
+
+    @Override
+    public void serviceGone(PluginService service) {
+	topicmapsService = null;
+    }
+
+
+}
--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -3,13 +3,14 @@ dm4c.add_plugin("net.abriraqui.import-export", function(){
     dm4c.add_listener("post_refresh_topicmap_menu", function(topicmap_menu) {
 	topicmap_menu.add_separator()
 	topicmap_menu.add_item({
-	    label: "Import Topicmap",
+	    label: "Export Topicmap",
 	    handler: import_topicmap
 	})
 	
 	function import_topicmap(){
 	    var topicmap = dm4c.get_plugin("de.deepamehta.topicmaps").get_topicmap()
-	    alert("Import topicmap" + topicmap.get_id())
+	    dm4c.restc.request("POST", "/import-export/export")
+	    alert("Export of topicmap " + topicmap.get_id() + " complete")
 	}
     })
 })
