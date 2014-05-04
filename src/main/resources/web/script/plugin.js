e1f6c67f6e37cc1fb5aa02379226f180829c1817
carolina <carolina@sindominio.net>
2014-05-04 13:06:05 +0200

    Add basic import_topicmap method to client

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -6,11 +6,22 @@ dm4c.add_plugin("net.abriraqui.import-export", function(){
 	    label: "Export Topicmap",
 	    handler: export_topicmap
 	})
+	topicmap_menu.add_separator()
+	topicmap_menu.add_item({
+	    label: "Import Topicmap",
+	    handler: import_topicmap
+	})
 	
 	function export_topicmap(){
 	    var topicmap = dm4c.get_plugin("de.deepamehta.topicmaps").get_topicmap()
 	    dm4c.restc.request("POST", "/import-export/export")
 	    alert("Export of topicmap " + topicmap.get_id() + " complete")
 	}
+
+	function import_topicmap(){
+	    alert("Import of topicmap " +  " complete")
+
+	}
+
     })
 })
