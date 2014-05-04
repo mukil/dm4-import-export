9bdf4a814d6fc6d17638dd445c6eedd41cafb5ee
carolina <carolina@sindominio.net>
2014-05-04 02:22:01 +0200

    Rename import method to export in plugin.js

--- src/main/resources/web/script/plugin.js
+++ src/main/resources/web/script/plugin.js
@@ -4,10 +4,10 @@ dm4c.add_plugin("net.abriraqui.import-export", function(){
 	topicmap_menu.add_separator()
 	topicmap_menu.add_item({
 	    label: "Export Topicmap",
-	    handler: import_topicmap
+	    handler: export_topicmap
 	})
 	
-	function import_topicmap(){
+	function export_topicmap(){
 	    var topicmap = dm4c.get_plugin("de.deepamehta.topicmaps").get_topicmap()
 	    dm4c.restc.request("POST", "/import-export/export")
 	    alert("Export of topicmap " + topicmap.get_id() + " complete")
